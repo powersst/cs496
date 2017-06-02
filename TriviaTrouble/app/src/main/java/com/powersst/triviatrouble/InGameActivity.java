361f2f536a726854676451d539bfc73bd85477bc
alexzdn <zadnipro@oregonstate.edu>
2017-06-01 18:55:10 -0700

    in_game_activity: needs on create finished

--- TriviaTrouble/app/src/main/java/com/powersst/triviatrouble/InGameActivity.java
+++ TriviaTrouble/app/src/main/java/com/powersst/triviatrouble/InGameActivity.java
@@ -1,5 +1,8 @@
 package com.powersst.triviatrouble;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,12 +18,27 @@ public class InGameActivity extends AppCompatActivity {
     private Button mBtnOptTwo;
     private Button mBtnOptThree;
     private Button mBtnOptFour;
-    private Button mBtnTrue;
-    private Button mBtnFalse;
-    private Button mBtnSubmitMult;
-    private Button mBtnSubmitDec;
+    private Button mBtnSubmit;
 
-    //need onCreate
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_game);
+
+        mQuestionTV = (TextView)findViewById(R.layout.questionTV);
+        mBtnOptOne = (Button)findViewById(R.layout.optionOneBtn);
+        mBtnOptTwo = (Button)findViewById(R.layout.optionTwoBtn);
+        mBtnOptThree = (Button)findViewById(R.layout.optionThreeBtn);
+        mBtnOptFour = (Button)findViewById(R.layout.optionFourBtn);
+        mBtnSubmit = (Button)findViewById(R.layout.submitBtn);
+
+        Intent intent = getIntent();
+
+
+    }
+
+
+//need onCreate
     //--pull content from intent
     //--create recycler items for each question
     //---randomize correct and incorrect answers
